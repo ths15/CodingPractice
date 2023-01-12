af32b94c23f08e27588f7a231d2560602be4edd5
ths15 <ths15@students.uwf.edu>
2023-01-11 20:40:04 -0600

    Completed

--- src/DailyCoding1.java
+++ src/DailyCoding1.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class DailyCoding1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int k = 17;
+        int [] array = {10, 15, 3, 7};
+        boolean acceptedArray = false;
+
+        System.out.println("List of numbers: " + Arrays.toString(array) + "\n"
+        + "Target of addition: " + k);
+
+        for(int i = 0; i < array.length - 1; i++)
+        {
+            for(int j = i + 1; j < array.length; j++)
+            {
+                if(array[i] + array[j] == k)
+                {
+                    acceptedArray = true;
+                    break;
+                }
+            }
+
+            if(acceptedArray)
+                break;
+        }
+
+        if(acceptedArray)
+            System.out.println("This list is accepted");
+        else
+            System.out.println("This list is not accepted");
+
     }
 }
